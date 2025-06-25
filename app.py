3e5623f567de612bb5f18616d190b77ec10fd476
garyteng2021 <garyteng2021@gmail.com>
2025-06-25 17:39:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, request, jsonify
+import os
+import random
+import psycopg2
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+def get_conn():
+    return psycopg2.connect(DATABASE_URL)
 
 @app.route("/api/user/bind", methods=["POST"])
 def bind():
