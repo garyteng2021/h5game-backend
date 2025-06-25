173017da5eea8b7f44f1056d9277e93e4d75b662
garyteng2021 <garyteng2021@gmail.com>
2025-06-25 15:47:46 +0800

    Add files via upload

--- main_flask.py
+++ main_flask.py
@@ -1,6 +1,11 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello, world!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 8080)))
